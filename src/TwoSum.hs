57dc7bff48a78d832df371eb82db29046beedf00
Gary Zhao <gzhao@groupon.com>
2014-09-20 12:09:14 -0700

    changes

--- src/TwoSum.hs
+++ src/TwoSum.hs
@@ -5,8 +5,8 @@ module TwoSum
 
 twoSum :: [Int]->Int->(Int,Int)
 twoSum numbers target = [(x,y) | x<-r, y<-r, x<y && numbers !! x + numbers !! y == target] !! 0
-        where r = [0..length numbers-1]
+    where r = [0..length numbers-1]
 
 test = twoSum xs t
-        where   xs = [2, 7, 11, 15]
-                t = 9
+    where   xs = [2, 7, 11, 15]
+            t = 9
